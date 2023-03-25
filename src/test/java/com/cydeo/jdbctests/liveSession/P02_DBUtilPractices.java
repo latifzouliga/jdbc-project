acd3fa2cc2d54f64f850b0a8e962717732cd7ddb
mehmetCt <mehmet@cybertekschool.com>
2023-03-25 12:01:57 -0400

    Live Session

---
+++ src/test/java/com/cydeo/jdbctests/liveSession/P02_DBUtilPractices.java
@@ -0,0 +1,40 @@
+package com.cydeo.jdbctests.liveSession;
+
+import org.junit.jupiter.api.Test;
+
+public class P02_DBUtilPractices {
+
+    @Test
+    public void dbUtil() {
+        System.out.println("GET ME ROW COUNT ");
+
+        System.out.println("GET ME COLUMN COUNT ");
+
+
+        System.out.println("GET ALL COLUMN NAME AS LIST");
+
+
+        System.out.println("GET ROW DATA AS LIST");
+
+
+        System.out.println("GET CELL VALUE --> int rowNum , int columnIndex");
+
+
+        System.out.println("GET CELL VALUE --> int rowNum ,String columnName");
+
+
+        System.out.println("GET FIRST ROW FIRST COLUMN VALUE");
+
+
+        System.out.println("GET COLUMN DATA AS LIST--> int columnNum");
+
+
+        System.out.println("GET COLUMN DATA AS LIST--> String columnName");
+
+
+        System.out.println("GET ROW MAP");
+
+
+        System.out.println("GET ALL ROW AS LIST OF MAP");
+    }
+}
