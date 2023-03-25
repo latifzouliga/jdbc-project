66fad98116ed11bf9ccb5623335ed659a8ca2d9c
mehmetCt <mehmet@cybertekschool.com>
2023-03-25 10:41:13 -0400

    Live Session

---
+++ src/test/java/com/cydeo/jdbctests/liveSession/P01_JDBCPractice.java
@@ -0,0 +1,14 @@
+package com.cydeo.jdbctests.liveSession;
+
+import org.junit.jupiter.api.Test;
+
+public class P01_JDBCPractice {
+
+    String dbUrl="jdbc:oracle:thin:@54.237.226.155:1521:XE";
+    String dbPassword="hr";
+    String dbUsername="hr";
+
+    @Test
+    public void jdbcPractice() {
+    }
+}
